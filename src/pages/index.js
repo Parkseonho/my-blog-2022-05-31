621243f63b8f07e05762707f3644fdf9c670bb6d
Parkseonho <tjsgh2103@gmail.com>
2022-05-31 12:35:18 +0900

    페이지 넘김2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <main>
       <title> home page?? </title>
       <h1> home page!! </h1>
+      <Link to="/about">About</Link>
     </main>
   );
 };
