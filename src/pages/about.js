e68938d21cfe8639d3ba6b6a785113071698f742
Parkseonho <tjsgh2103@gmail.com>
2022-05-31 13:12:32 +0900

    레이아웃 삭제

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
-import Layout from "../components/layout";
 
 function aboutpage() {
   return (
     <main>
       <title>자기소개</title>
       <h1>자기소개 페이지 입니다.</h1>
+
       <Link to="/">Back to home</Link>
-      <Layout pageTitle="About me">
-        <p>
-          Hi there! I'm the proud creator of this site, which I built with
-          Gatsby.
-        </p>
-      </Layout>
     </main>
   );
 }
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { Link } from "gatsby";
-import Layout from "../components/layout";
 
 function testpage() {
   return (
     <main>
-      <Layout pageTitle="테스트 Page">
-        <p>테스트임</p>
-      </Layout>
       <title>test page</title>
       <h1>테스트 페이지 입니다.</h1>
       <Link to="/">Back to home</Link>
