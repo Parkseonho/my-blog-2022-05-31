48e25e8c7cce0bef1b6c27a73953d12d59494c65
Parkseonho <tjsgh2103@gmail.com>
2022-06-01 11:29:46 +0900

    다른페이지에 사진 추가

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 function AboutPage() {
   return (
     <Layout pageTitle="자기소개">
       <p>자기소개 페이지 입니다.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/cat.jpg"
+      />
     </Layout>
   );
 }
