89ce6d39230f2ee469a418fda0dac6435e043a55
Parkseonho <tjsgh2103@gmail.com>
2022-06-01 11:08:45 +0900

    그래프 QL로 데이터 가져와서 레이아웃에 표시

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Site",
+    description: "해당 사이트엔 박선호의 개발일지가 담겨있습니다.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
