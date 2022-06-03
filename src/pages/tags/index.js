4f05122e0396618778ce8f12753317d1c306fc57
Parkseonho <tjsgh2103@gmail.com>
2022-06-03 11:56:55 +0900

    태그 표시용 페이지 제작

---
+++ src/pages/tags/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+function TagtPage() {
+  return (
+    <Layout pageTitle="태그페이지">
+      <p>태크 페이지 입니다.</p>
+    </Layout>
+  );
+}
+export default TagtPage;
